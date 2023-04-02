855fbde42b87d4a49fb0f48eb4c188acefc6da7d
Yew Jin <yewjin@example.com>
2023-04-02 10:59:23 -0700

    add ANSI color to responses and also optimize messages if the context gets too long

--- main.py
+++ main.py
@@ -72,6 +72,46 @@ def format_allowed_commands(commands_dict):
 spacy_commands_str = format_allowed_commands(ALLOWED_SPACY_COMMANDS)
 nltk_commands_str = format_allowed_commands(ALLOWED_NLTK_COMMANDS)
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def optimize_messages(messages):
+    condensed_messages = []
+
+    # Remove system message from input
+    #messages_without_system = [msg for msg in messages if msg["role"] != "system"]
+
+    # Concatenate all user and assistant messages
+    #combined_messages = " ".join([f"{msg['role']}:{msg['content']}" for msg in messages_without_system])
+    combined_messages = " ".join([f"{msg['role']}:{msg['content']}" for msg in messages])
+
+    # Summarize user and assistant messages together
+    summary = api.generate_response([
+        {"role": "system", "content": f"""You are an AI language model trained to understand and generate text based on user input. Your task now is to optimize the conversation by summarizing and condensing the messages received so far, while maintaining the context and crucial information. The original system message is passed to you for context, do not repeat it. Based on your understanding of the conversation, decide the best way to present the condensed information back to the GPT model. You can provide one or more summary messages with either a 'user' or 'assistant' role. Do not repeat the user task or the system message. Ensure that the condensed messages allow the GPT model to effectively correct and solve the problem."""},
+        {"role": "assistant", "content": combined_messages},
+        {"role": "user", "content": "Summarize the following conversation while maintaining its structure:"}], model="gpt-4")
+
+    # Split the summary into separate messages based on their roles
+    for message in summary.split("\n"):
+        if message.startswith("User:") or message.startswith("user:"):
+            condensed_messages.append({"role": "user", "content": message[5:].strip()})
+        elif message.startswith("Assistant:") or message.startswith("assistant:"):
+            condensed_messages.append({"role": "assistant", "content": message[10:].strip()})
+
+    return condensed_messages
+
+def print_messages(messages):
+    for msg in messages:
+        print(f"{msg['role'].capitalize()}: {msg['content']}")
+
 messages = [
     {"role": "system",
      "content": f"""You are an AI language model trained to understand and generate text based on user input. Your task is to generate responses that follow the constraints provided by the user. If the user specifies constraints on the text such as word count, character limits, or restrictions on punctuation, letters, or word order, incorporate those constraints into your response. Always try to provide a coherent and relevant answer to the user's question.
@@ -105,17 +145,39 @@ When you have verified that your response meets the constraints using the provid
      "content": "FINAL ANSWER: Spring brings new life, Flowers bloom, birds sing, Warm sunshine, new beginnings."},
 ]
 
+original_messages_len = len(messages)
+messages_seen_by_assistant = []
+
 while True:
     user_input = input("User: ").strip()
 
     if user_input == "exit": break
       
     messages.append({"role": "user", "content": f"{user_input}"})
+    messages_seen_by_assistant = messages.copy()
     num_iterations = 0
     temperature = 0.0
     frequency_penalty = 0.0
-    while num_iterations < 50:
-      response = api.generate_response(messages, temperature=temperature, frequency_penalty=frequency_penalty, model="gpt-4")
+    while num_iterations < 100:
+      if len(messages_seen_by_assistant) > 30:
+          context = [messages[0]] + messages[original_messages_len:]
+          condensed_messages = optimize_messages(context)
+          command_message = ""
+          for m in reversed(messages):
+              if m['role'] == 'assistant' and ('EXECUTE SPACY:' in m['content'] or 'EXECUTE NLTK:' in m['content']):
+                  command_message = m['content']
+                  break
+          user = f"Your last response with a command is '{command_message}'. Use this info and think step-by-step. Confirm that the solution meets my task"
+          condensed_messages.append({"role":"user", "content":user})
+ 
+          print("=========================================")
+          print("OPTIMIZING MESSAGES:")
+          print("=========================================")
+          print_messages(condensed_messages)
+          print("=========================================")
+          messages_seen_by_assistant = messages[:original_messages_len+1] + condensed_messages
+
+      response = api.generate_response(messages_seen_by_assistant, temperature=temperature, frequency_penalty=frequency_penalty, model="gpt-4")
       temperature = 0.0
       frequency_penalty = 0.0
       if response.startswith("FINAL ANSWER:"):
@@ -126,38 +188,42 @@ while True:
 
       # Check for spaCy or nltk command requests
       if response.startswith("EXECUTE SPACY:"):
-        print(response)
         command_str = response[len("EXECUTE SPACY:"):].strip()
         if "EXECUTE SPACY:" not in command_str and "EXECUTE NLTK:" not in command_str:
+            print(f"{bcolors.OKBLUE}{response}{bcolors.ENDC}")
             result = execute_spacy_command(command_str)
             print("RESULT:", result)
             messages.append({"role": "assistant", "content": f"Result of spaCy command: {result}"})
+            messages_seen_by_assistant.append({"role": "assistant", "content": f"Result of spaCy command: {result}"})
             thinking = False
       elif response.startswith("EXECUTE NLTK:"):
-        print(response)
         command_str = response[len("EXECUTE NLTK:"):].strip()
         if "EXECUTE SPACY:" not in command_str and "EXECUTE NLTK:" not in command_str:
+            print(f"{bcolors.OKBLUE}{response}{bcolors.ENDC}")
             result = execute_nltk_command(command_str)
             print("RESULT:", result)
             messages.append({"role": "assistant", "content": f"Result of nltk command: {result}"})
+            messages_seen_by_assistant.append({"role": "assistant", "content": f"Result of nltk command: {result}"})
             thinking = False
       
       if thinking:
         assistant = response
-        print("THINKING:", assistant)
+        if not response.startswith("THINKING:"):
+          assistant = f"THINKING: {response}"
+        print(f"""{bcolors.WARNING}{assistant}{bcolors.ENDC}""")
         user = "Continue."
         if "EXECUTE SPACY:" in assistant or "EXECUTE NLTK:" in assistant:
             user += " Remember if you are issuing a command it has to be the only text in your assistant. Do not apologize, just issue the command in next response. If you have more than one command, issue the first one next."
 
         if "Result of spaCY command:" in assistant or "Result of nltk command:" in assistant:
             temperature = 0.8
-            parts = []
+            parts = ["THINKING:"]
             command_message = ""
             for m in reversed(messages):
                 if m['role'] == 'assistant' and ('EXECUTE SPACY:' in m['content'] or 'EXECUTE NLTK:' in m['content']):
                     command_message = m['content']
                     break
-            user += f" Do not anticipate results. Reissue the command you gave in your last response '{messages[-1]}'. Do not apologize, just issue the command in next response. If you have more than one command, issue the first one next."
+            user += f" Do not anticipate results. Reissue the command you gave in your last response '{command_message}'. Do not apologize, just issue the command in next response. If you have more than one command, issue the first one next."
             if "Result of spaCY command:" in assistant:
                 parts.append("Result of spaCY command: <FAKE>")
             if "Result of nltk command:" in assistant:
@@ -166,7 +232,9 @@ while True:
 
 
         messages.append({"role": "assistant", "content": assistant})
+        messages_seen_by_assistant.append({"role": "assistant", "content": assistant})
         messages.append({"role": "user", "content": user})
+        messages_seen_by_assistant.append({"role": "user", "content": user})
 
       num_iterations += 1
 
